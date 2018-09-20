ca44406d32ed6236d132e0dd5bb0068ed3952562
michaeledander1 <michaeledander1@gmail.com>
2018-09-20 12:20:24 +0200

    bigger board

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -2,14 +2,17 @@ import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, M
 import User from '../users/entity'
 
 export type Symbol = 'x' | 'o' | 'c' | 'a' | '-' | 'X' | 'O'
-export type Row = [ Symbol | null, Symbol | null, Symbol | null ]
-export type Board = [ Row, Row, Row, Row, Row ]
+export type Row = [ Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null ]
+export type Board = [ Row, Row, Row, Row, Row, Row, Row, Row, Row, Row]
 
-const cupRowCenter: Row = [null, 'c', null]
-const cupRowLeft: Row = ['c', null, null]
-const cupRowRight: Row = [null, null,  'c']
-const emptyRow: Row = [null, null, null]
-const emptyBoard: Board = [ cupRowLeft, emptyRow, cupRowRight, emptyRow, cupRowCenter ]
+const cupRowFarFarRight: Row = [null, null, null, null, 'c']
+const cupRowFarRight: Row = [null, null, null, 'c', null]
+const cupRowCenter: Row = [null, 'c', null, null, null]
+const cupRowLeft: Row = ['c', null, null, null, null]
+const cupRowRight: Row = [null, null,  'c', null, null]
+const emptyRow: Row = [null, null, null, null, null]
+const emptyBoard: Board = [ cupRowLeft, emptyRow, cupRowRight, emptyRow, cupRowFarRight, emptyRow, 
+  emptyRow, cupRowFarFarRight, cupRowCenter, emptyRow ]
 
 type Status = 'pending' | 'started' | 'finished'
 
