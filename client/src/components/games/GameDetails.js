7fce6298cafe7aafad0f153411657d44d3749aae
michaeledander1 <michaeledander1@gmail.com>
2018-09-21 11:18:22 +0200

    conditional rendering set for cups

--- client/src/components/games/GameDetails.js
+++ client/src/components/games/GameDetails.js
@@ -24,6 +24,17 @@ class GameDetails extends PureComponent {
     const { players } = game
     const currentPlayer = players.filter(p => p.symbol === game.turn)
     const currentTurnPlayer = currentPlayer[0]
+    // const turnLength = setInterval(turnTimer, 3000);
+      
+    // turnTimer = () => {
+    //     if (game.turn === 'x') {
+    //       return game.turn === 'o'
+    //     }
+    //     if (game.turn === 'o') {
+    //       return game.turn === 'x'
+    //     }
+    //   }
+    
 
     const board = game.board.map(
       (row, rowIndex) => row.map((cell, cellIndex) => {
@@ -42,38 +53,40 @@ class GameDetails extends PureComponent {
     updateGame(game.id, board)
     // this.drinkBeer(game.board)
   }
-
-  // drinkBeer = (board) => {
-  //   const winnerScoreX = board
-  //     .map(array => array.includes('X'))
-  //     .filter(trueOrFalse => trueOrFalse)
-  //     .length
-  //   const winnerScoreO = board
-  //     .map(array => array.includes('O'))
-  //     .filter(trueOrFalse => trueOrFalse)
-  //     .length
-  //   if (winnerScoreX === 1) {
-  //     return <div>
-  //       Drink your beer!
-  //       <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
-  //     </div>
-  //   } if (winnerScoreX === 2) {
-  //     return <div>
-  //       Drink again! Watch out, if that loser across from you gets one more in a cup, you lose!
-  //       <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
-  //     </div>
-  //   } if (winnerScoreO === 1) {
-  //     return <div>
-  //       Drink your beer!
-  //       <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
-  //     </div>
-  //   } if (winnerScoreX === 2) {
-  //     return <div>
-  //       Drink again! Watch out, if that loser across from you gets one more in a cup, you lose!
-  //       <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
-  //     </div>
-  //   } else return null
-  // }
+  drinkBeerO = (board) => {
+    const winnerScoreX = board
+      .map(array => array.includes('X'))
+      .filter(trueOrFalse => trueOrFalse)
+      .length  
+    if (winnerScoreX === 1) {
+      return <div>
+        Drink your beer!
+        <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
+      </div>
+    } if (winnerScoreX === 2) {
+      return <div>
+        Drink again! Watch out, if that loser across from you gets one more in a cup, you lose!
+        <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
+      </div>
+    } else return null
+  }
+  drinkBeerX = (board) => {
+    const winnerScoreO = board
+      .map(array => array.includes('O'))
+      .filter(trueOrFalse => trueOrFalse)
+      .length
+    if (winnerScoreO === 1) {
+      return <div>
+        The other player just hit a cup! Drink up buddy!
+        <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
+      </div>
+    } if (winnerScoreO === 2) {
+      return <div>
+        Drink again! Watch out, if that loser across from you gets one more cup, you lose!
+        <img alt='beer' src={'https://cdn1.wine-searcher.net/images/labels/90/80/heineken-lager-beer-amsterdam-netherlands-10519080.jpg'}/>
+      </div>
+    } else return null
+  }
 
 
 
@@ -94,8 +107,10 @@ class GameDetails extends PureComponent {
     const winner = game.players
       .filter(p => p.symbol === game.winner)
       .map(p => p.userId)[0]
-
-    // const loser = game.players.symbol != game.turn
+    
+    const loser = game.players
+      .filter(p => p.symbol !== game.winner)
+      .map(p => p.userId)[0]
 
     return (<Paper className="outer-paper">
       {
@@ -121,23 +136,34 @@ class GameDetails extends PureComponent {
             <br/>
             The loser downs his beer!!
           </p>
-          {/* {this.drinkBeer()} */}
         </div>
       }
       <br/>
       {
         game.status === 'started' &&
-        player && player.symbol === game.turn &&
-        <div>Go alreadyyyyyyyyy!</div>
+        player.symbol === 'x' &&
+        <div>{this.drinkBeerX(this.props.game.board)}</div>
       }
-      
+
       {
-        game.board.includes
+        game.status === 'started' &&
+        player.symbol === 'o' &&
+        <div>{this.drinkBeerO(this.props.game.board)}</div>
+      }
+
+      {
+        game.status === 'started' &&
+        player && player.symbol === game.turn &&
+        <div>
+          <p>Go alreadyyyyyyyyy!</p>
+        </div>
       }
       {
         game.status === 'started' &&
         player.symbol !== game.turn &&
-        <div>Wait your turn bro...</div>
+        <div>
+        <p>Wait your turn bro...</p>
+        </div>
       }
 
       {
@@ -150,11 +176,9 @@ class GameDetails extends PureComponent {
         winner &&
         <div>
           <p>yooOOooOo {users[winner].firstName}, you won bro</p>
-          {/* <p>{users[loser].firstName}, drink up!</p> */}
+          <p>{users[loser].firstName}, you lose! Drink up!</p>
         </div>
       }
-
-      <hr />
 
       {
         game.status !== 'pending' &&
